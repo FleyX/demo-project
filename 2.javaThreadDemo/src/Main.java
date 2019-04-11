abfc0fbabc3ace84fda5762547f536d9b4d02b22
fanxb <fanxb@szinfinova.com>
2019-04-11 16:39:14 +0800

    update main

--- 2.javaThreadDemo/src/Main.java
+++ 2.javaThreadDemo/src/Main.java
@@ -1,8 +1,22 @@
+import java.util.concurrent.*;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("当前线程名为："+Thread.currentThread().getName());
-        System.out.println("当前线程id为："+Thread.currentThread().getId());
+        System.out.println("当前线程名为：" + Thread.currentThread().getName());
+        System.out.println("当前线程id为：" + Thread.currentThread().getId());
+    }
+}
+
+class Item implements Delayed {
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        return 0;
     }
 }
