419335158e2ac03c891c4230cb0e08781f1e9a12
fanxb <fleyx20@outlook.com>
2021-07-07 11:22:45 +0800

    add

---
+++ 5.leetcode/src/com/fanxb/common/Q1833.java
@@ -0,0 +1,29 @@
+package com.fanxb.common;
+
+import java.util.*;
+
+/**
+ * 两数相加
+ *
+ * @author fanxb
+ * @date 2021/6/1
+ **/
+public class Q1833 {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for (int i = 0; i < costs.length; i++) {
+            coins -= costs[i];
+            if (coins >= 0) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Q1833().maxIceCream(new int[]{1, 3, 2, 4, 1}, 7));
+    }
+}
