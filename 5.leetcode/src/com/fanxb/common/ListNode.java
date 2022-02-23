7c8b2ca1d003c9a89281ccfa7f6d56c92c34d5d4
fanxb <fleyx20@outlook.com>
2022-02-23 11:24:00 +0800

    add

---
+++ 5.leetcode/src/com/fanxb/common/ListNode.java
@@ -0,0 +1,18 @@
+package com.fanxb.common;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public ListNode() {
+    }
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
