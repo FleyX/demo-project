121cc9b5ba1569865fac660edba912d98208cf91
fanxb <fanxb.tl@gmail.com>
2024-03-27 23:46:18 +0800

    add

---
+++ 5.leetcode/src/com/fanxb/common/Q130.java
@@ -0,0 +1,35 @@
+package com.fanxb.common;
+
+public class Q130 {
+    public void solve(char[][] board) {
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isBoard = i == 0 || i == m - 1 || j == 0 || j == n - 1;
+                if (isBoard && board[i][j] == 'O') {
+                    dfs(board, i, j, m, n);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if(board[i][j]=='0') board[i][j]='O';
+            }
+        }
+    }
+
+    /**
+     * 是否有边上的
+     *
+     * @return
+     */
+    private void dfs(char[][] board, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] == 'X' || board[i][j] == '0') return;
+        board[i][j] = '0';
+        dfs(board, i + 1, j, m, n);
+        dfs(board, i - 1, j, m, n);
+        dfs(board, i, j + 1, m, n);
+        dfs(board, i, j - 1, m, n);
+    }
+}
---
+++ 5.leetcode/src/com/fanxb/common/Q207.java
@@ -0,0 +1,37 @@
+package com.fanxb.common;
+
+import java.util.*;
+
+public class Q207 {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //记录课程的入度
+        int[] inCount = new int[numCourses];
+        //记录课程的后续课程
+        Map<Integer, List<Integer>> nextCourses = new HashMap<>(numCourses);
+        for (int[] pre : prerequisites) {
+            inCount[pre[0]]++;
+            List<Integer> next = nextCourses.computeIfAbsent(pre[1], k -> new ArrayList<>());
+            next.add(pre[0]);
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inCount[i] == 0) queue.offer(i);
+        }
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int c = queue.poll();
+            count++;
+            List<Integer> next = nextCourses.get(c);
+            if (next == null) continue;
+            next.forEach(item -> {
+                inCount[item]--;
+                if (inCount[item] == 0) queue.offer(item);
+            });
+        }
+        return numCourses == count;
+    }
+
+    public static void main(String[] args) {
+        new Q207().canFinish(2, new int[][]{{0, 1}});
+    }
+}
